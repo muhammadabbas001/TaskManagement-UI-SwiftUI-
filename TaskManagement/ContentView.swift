2c7ee438a7a14ff541b939ab4f984509d4343513
Muhammad Abbas <muhammdabbas.cs@gmail.com>
2022-01-14 20:46:27 +0500

    Initial Commit

---
+++ TaskManagement/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TaskManagement
+//
+//  Created by Muhammad Abbas on 14/01/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TaskManagement/TaskManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagementApp.swift
+//  TaskManagement
+//
+//  Created by Muhammad Abbas on 14/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
